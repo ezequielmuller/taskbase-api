cc8233ea7effbe7205fdcd0a15059f269db8705b
Computação <zikimuller017@gmail.com>
2026-03-17 22:15:28 -0300

    feat(connection): conexao com o banco de dados postgresql

--- src/main/java/com/taskflow/persistencia/Conexao.java
+++ src/main/java/com/taskflow/persistencia/Conexao.java
@@ -6,20 +6,13 @@ import java.sql.SQLException;
 
 public class Conexao {
 
-  private static final String URL = "jdbc:postgresql://localhost:5432/taskflow-database";
+  private static final String URL = "jdbc:postgresql://localhost:5432/taskflow_database";
   private static final String USUARIO = "postgres";
   private static final String SENHA = "123";
 
-  public static Connection conectar() {
-    Connection con = null;
-
-    try {
-      con = DriverManager.getConnection(URL, USUARIO, SENHA);
-      System.out.println("Banco de dados conectado com sucesso");
-    } catch (SQLException e) {
-      System.out.println("Erro ao conectar no banco de dados: " + e.getMessage());
-    }
-
+  public Connection conectar() throws SQLException {
+    Connection con = DriverManager.getConnection(URL, USUARIO, SENHA);
+    System.out.println("Banco de dados postgres conectado =============");
     return con;
   }
 }
